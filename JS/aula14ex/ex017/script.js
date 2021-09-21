a27e6ecf5dd046d85a0f8993c805f7787a6ba40e
Daiane <daianespielmann9@gmail.com>
2021-09-21 19:32:49 -0300

    ex017

--- JS/aula14ex/ex017/script.js
+++ JS/aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function tabuada() {
+  var numero = document.querySelector("input#numero");
+  var tabuada = document.querySelector("select#select_tabuada");
+
+  if (numero.value.length == 0) {
+    window.alert("Por favor, digite um número!");
+  } else {
+    var num = Number(numero.value);
+    var c = 1;
+    tabuada.innerHTML = "";
+    while (c <= 10) {
+      var item = document.createElement("option");
+      item.text = `${num} x ${c} = ${num * c}`;
+      item.value = `tabuada ${c}`;
+      tabuada.appendChild(item);
+      c++;
+    }
+  }
+}
